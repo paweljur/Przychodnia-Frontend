cc706f5c4d09c9fcfee90424223e9a3802276e83
Paweł Jur <pawel.tomasz.jur@gmail.com>
2020-01-07 17:07:06 +0100

    Default navigation after login

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { AuthenticationService } from 'src/core/services/authentication.service';
 import { LoggedInUserDto } from 'src/core/api/service-proxies';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -18,10 +19,31 @@ export class MainComponent implements OnInit {
 
   currentUser: LoggedInUserDto;
 
-  constructor(private _breakpointObserver: BreakpointObserver, private _authenticationService: AuthenticationService) {}
+  constructor(
+    private _breakpointObserver: BreakpointObserver,
+    private _authenticationService: AuthenticationService,
+    private _router: Router
+  ) {}
 
   ngOnInit(): void {
     this.currentUser = this._authenticationService.currentUser;
+
+    switch (this.currentUser.role) {
+      case 'admin':
+        this._router.navigate(['/users']);
+        break;
+      case 'registrant':
+        this._router.navigate(['/appointments']);
+        break;
+      case 'doctor':
+        this._router.navigate(['/my-appointments']);
+        break;
+      case 'laborant':
+        this._router.navigate(['/test-orders']);
+        break;
+      default:
+        break;
+    }
   }
 
   logout(): void {
